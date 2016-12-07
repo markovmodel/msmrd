2ebf8050273ae4224599e2372a88d948c421fc56
Manuel Dibak <manuel@dibak.de>
2016-12-07 12:50:36 +0100

    added frompath method

--- MSMRD/MSM.py
+++ MSMRD/MSM.py
@@ -2,9 +2,9 @@ import numpy as np
 from .utils import voronoi
 
 class MSM:
-    def __init__(self, path, exitR = 0., MSMradius=0., lagtime=1):
-        self.T = np.loadtxt(path + '_Tmatrix.txt')
-        self.centers = np.loadtxt(path + '_centers.txt')
+    def __init__(self, T, centers, exitR = 0., MSMradius=0., lagtime=1):
+        self.T = T
+        self.centers = centers
         self.MSMradius=MSMradius
         self.lagtime = lagtime
         self.states = self.T.shape[0]
@@ -13,6 +13,11 @@ class MSM:
         #self.entryStates = np.where(np.logical_and(np.linalg.norm(self.centers, axis=1) < entryRadii[1], np.linalg.norm(self.centers,axis=1) > entryRadii[0]))[0]
         self.state = -1
         self.exit = False
+
+    def frompath(self, path, exitR = 0., MSMradius=0., lagtime=1):
+        T = np.loadtxt(path + '_Tmatrix.txt')
+        centers = np.loadtxt(path + '_centers.txt')
+        return self(T, centers, exitR, MSMradius, lagtime)
 
     def propagate(self):
         self.state = np.random.choice(self.states, p=self.T[self.state])

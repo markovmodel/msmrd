42f6496b0d44f4d9e24cd34da80ff5eaef3d16ff
Del Razo Sarmina Mauricio <maojrs@cheetah.imp.fu-berlin.de>
2017-01-31 13:59:30 +0100

    Added comment

--- MSMRD/potentials/asym2Dpotential.py
+++ MSMRD/potentials/asym2Dpotential.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+# Construct asymmetric 2D potential from Gaussian, calculate gradients and plots.
 class asym2Dpotential(object):
     def __init__(self, minima=None, sigmas=None):
         if minima == None:

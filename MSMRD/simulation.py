06b90532db15afd52f354f50784e14bb5108916a
Del Razo Sarmina Mauricio <maojrs@cheetah.imp.fu-berlin.de>
2017-02-01 11:12:26 +0100

    Added run_n_buffer function

--- MSMRD/simulation.py
+++ MSMRD/simulation.py
@@ -16,6 +16,39 @@ class simulation:
                     j = i / samplingInterval
                     self.traj[j,:] = self.integrator.sample(i)
 
+    def run_n_buffer(self, steps, sample = False, samplingInterval = 1, \
+	             filename="../data/trajs_data.txt", buffersize = int(1e5)):
+	numbuffer = 0
+	bufcounter = 0
+        samples = int(steps / samplingInterval) + 1
+        self.traj = np.zeros((buffersize, self.integrator.sampleSize))
+	# open blank file and allow appending for buffer mode	
+	open(filename, "w")
+	f = open(filename, "a")
+	# loop over all steps and write file when buffer is filled
+        for i in range(steps):
+            self.integrator.integrate()
+            if sample:
+                if not i % samplingInterval:
+		    bufcounter = bufcounter + 1 
+                    j = i / samplingInterval - numbuffer*buffersize
+                    self.traj[j,:] = self.integrator.sample(i)
+	    # empty to file when buffer is full and reinitialize traj array
+	    if (bufcounter == buffersize):
+	    	bufcounter = 0
+		numbuffer = numbuffer + 1
+		np.savetxt(f, self.traj)
+                a = [1,2,3]
+		np.savetxt(f, a)
+		ransteps = numbuffer*buffersize*samplingInterval
+		if (steps - ransteps) < (buffersize*samplingInterval):
+			#lastarray = int((steps - ransteps)/samplingInterval) + 1
+			lastarray = samples%buffersize
+			self.traj = np.zeros((lastarray, self.integrator.sampleSize))
+		else:
+			self.traj = np.zeros((buffersize, self.integrator.sampleSize))
+	np.savetxt(f,self.traj)
+
     def run_mfpt(self, threshold):
         i = 0
         while self.integrator.above_threshold(threshold):

f4e1b5e2d1d47d78b64042d62d73db3ea8f8109c
Del Razo Sarmina Mauricio <maojrs@cheetah.imp.fu-berlin.de>
2017-02-07 11:41:27 +0100

    Modified parameters for another run

--- scripts/create_asymTrajectories.py
+++ scripts/create_asymTrajectories.py
@@ -7,17 +7,17 @@ from multiprocessing import Process
 def run_simulation(runNumber):
     global correlation, runtimes
     np.random.seed()
-    runtime = int(1e7)
+    runtime = int(1e6)
     asympot = potentials.asym2Dpotential()
     x0 = 4.0*np.random.rand() - 2.0
     y0 = 4.0*np.random.rand() - 2.0
     r1 = np.array([x0, y0])
     p1 = mrd.particle(r1, 1.0)
-    box = mrd.box(2.)
+    box = mrd.box(4.)
     integrator = integrators.brownianDynamicsSp(asympot, box, p1, 0.0001, 1.0)
     sim = mrd.simulation(integrator)
     outfile = '../data/asym2D/2DasymTrajsLong_'+ str(runNumber)+ '.txt'
-    sim.run_n_buffer(runtime, sample=True, samplingInterval=100, \
+    sim.run_n_buffer(runtime, sample=True, samplingInterval=10, \
                      filename = outfile, buffersize = int(1e5))
 
 #run_simulation(2)

Corrected force sign

MSMRD/potentials/asym2Dpotential.py
@@ -45,7 +45,7 @@ class asym2Dpotential(object):
             grady = grady/(2*np.pi*sigx*sigy)
             outx = outx - gradx
             outy = outy - grady
-        return np.array([outx,outy])
+        return -np.array([outx,outy])
 
     # Calculate norm of gradient of the potential
     def gradnorm(self,r):

scripts/create_asymTrajectories.py
@@ -7,8 +7,8 @@ from multiprocessing import Process
 def run_simulation(runNumber):
     global correlation, runtimes
     np.random.seed()
-    runtime = int(3e6)
-    asympot = potentials.asym2Dpotential()
+    runtime = int(1e7)
+    asympot = potentials.asym2Dpotential(scalefactor = 0.7)
     x0 = 4.0*np.random.rand() - 2.0
     y0 = 4.0*np.random.rand() - 2.0
     r1 = np.array([x0, y0])
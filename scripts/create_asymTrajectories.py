437d1b6f3ecf167d47e8d4bab66c98ccc75f8d98
Del Razo Sarmina Mauricio <maojrs@cheetah.imp.fu-berlin.de>
2017-02-14 15:48:11 +0100

    Run asymtrajs in spehere instead of box

--- scripts/create_asymTrajectories.py
+++ scripts/create_asymTrajectories.py
@@ -7,14 +7,14 @@ from multiprocessing import Process
 def run_simulation(runNumber):
     global correlation, runtimes
     np.random.seed()
-    runtime = int(1e7)
+    runtime = int(1e5)
     asympot = potentials.asym2Dpotential(scalefactor = 0.7)
     x0 = 4.0*np.random.rand() - 2.0
     y0 = 4.0*np.random.rand() - 2.0
     r1 = np.array([x0, y0])
     p1 = mrd.particle(r1, 1.0)
-    box = mrd.box(4.)
-    integrator = integrators.brownianDynamicsSp(asympot, box, p1, 0.0001, 1.0)
+    ringboundary = mrd.reflectiveRing(2.)
+    integrator = integrators.brownianDynamicsSp(asympot, ringboundary, p1, 0.0001, 1.0)
     sim = mrd.simulation(integrator)
     outfile = '../data/asym2D/2DasymTrajsLong_'+ str(runNumber)+ '.txt'
     sim.run_n_buffer(runtime, sample=True, samplingInterval=10, \

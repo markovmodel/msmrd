d868d4b00a5154478a17a74db0d0b83f0b8b8d35
Del Razo Sarmina Mauricio <maojrs@cheetah.imp.fu-berlin.de>
2017-02-20 16:28:47 +0100

    Fixed regionmap

--- scripts/create_asymTrajectories.py
+++ scripts/create_asymTrajectories.py
@@ -7,18 +7,18 @@ from multiprocessing import Process
 def run_simulation(runNumber):
     global correlation, runtimes
     np.random.seed()
-    runtime = int(1e6)
+    runtime = int(1e5)
     asympot = potentials.asym2Dpotential(scalefactor = 0.7)
     x0 = 2.0*np.random.rand() - 1.0
     y0 = 2.0*np.random.rand() - 1.0
     r1 = np.array([x0, y0])
     p1 = mrd.particle(r1, 1.0)
-    ringboundary = mrd.reflectiveRing(2.)
+    ringboundary = mrd.reflectiveRing(4.)
     integrator = integrators.brownianDynamicsSp(asympot, ringboundary, p1, 0.0001, 1.0)
     sim = mrd.simulation(integrator)
-    outfile = '../data/asym2D/2DasymTrajsLong_'+ str(runNumber)+ '.txt'
+    outfile = '../data/asym2D/2DasymTrajsLong_bigR_shortT_'+ str(runNumber)+ '.h5'
     sim.run_n_buffer(runtime, sample=True, samplingInterval=10, \
-                     filename = outfile, buffersize = int(1e5))
+                     filename = outfile, buffersize = int(1e4))
 
 #run_simulation(2)
 

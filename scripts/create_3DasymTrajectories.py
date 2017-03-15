75b602198f74a71b9fa5498960f9555b0be89d1a
Del Razo Sarmina Mauricio <maojrs@sesamsaat.imp.fu-berlin.de>
2017-03-15 17:24:37 +0100

    Change naming of 3 trajs

--- scripts/create_3DasymTrajectories.py
+++ scripts/create_3DasymTrajectories.py
@@ -7,17 +7,17 @@ from multiprocessing import Process
 def run_simulation(runNumber):
     global correlation, runtimes
     np.random.seed()
-    runtime = int(1e5)
+    runtime = int(1e7)
     asympot = potentials.asym3Dpotential(scalefactor = 0.7)
-    x0 = 2.0*np.random.rand() - 1.0
-    y0 = 2.0*np.random.rand() - 1.0
-    z0 = 2.0*np.random.rand() - 1.0
+    x0 = 4.0*np.random.rand() - 2.0
+    y0 = 4.0*np.random.rand() - 2.0
+    z0 = 4.0*np.random.rand() - 2.0
     r1 = np.array([x0, y0, z0])
     p1 = mrd.particle(r1, 1.0)
     sphereboundary = mrd.reflectiveSphere(4.)
-    integrator = integrators.brownianDynamicsSp(asympot, sphereboundary, p1, 0.0001, 1.0)
+    integrator = integrators.brownianDynamicsSp(asympot, sphereboundary, p1, 0.001, 1.0)
     sim = mrd.simulation(integrator)
-    outfile = '../data/asym3D/test_3DasymTrajs'+ str(runNumber)+ '.h5'
+    outfile = '../data/asym3D/3DasymTrajs_R4_RT1e7_dt001_si10_'+ str(runNumber)+ '.h5'
     sim.run_n_buffer(runtime, sample=True, samplingInterval=10, \
                      filename = outfile, buffersize = int(1e3))
 
@@ -25,8 +25,8 @@ def run_simulation(runNumber):
 
 processes = []
 print("Simulation started")
-for j in range(0, 1):
-    for i in range(0, 1):
+for j in range(0, 15):
+    for i in range(0, 4):
 	print("Process " + str(i+4*j) + " running")
         p = Process(target = run_simulation, args=(i+4*j,))
         p.start()

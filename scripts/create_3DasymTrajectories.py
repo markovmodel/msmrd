798f63ed652e358a7999f3be6ffdc7d2841432e2
maojrs <maojrs@gmail.com>
2017-05-15 11:32:58 +0200

    Updated create_3Dtrjas script

--- scripts/create_3DasymTrajectories.py
+++ scripts/create_3DasymTrajectories.py
@@ -8,17 +8,20 @@ def run_simulation(runNumber):
     global correlation, runtimes
     np.random.seed()
     runtime = int(1e7)
-    asympot = potentials.asym3Dpotential(scalefactor = 2.0)
+    dt = 0.001
+    scalef = 2.0
+    sampInterval = 10
+    asympot = potentials.asym3Dpotential(scalefactor = scalef)
     x0 = 4.0*np.random.rand() - 2.0
     y0 = 4.0*np.random.rand() - 2.0
     z0 = 4.0*np.random.rand() - 2.0
     r1 = np.array([x0, y0, z0])
     p1 = mrd.particle(r1, 1.0)
-    sphereboundary = mrd.reflectiveSphere(4.)
-    integrator = integrators.brownianDynamicsSp(asympot, sphereboundary, p1, 0.001, 1.0)
+    boundary = mrd.box(5.0,3) #mrd.reflectiveSphere(4.)
+    integrator = integrators.brownianDynamicsSp(asympot, boundary, p1, dt, 1.0)
     sim = mrd.simulation(integrator)
-    outfile = '../data/asym3D/test3DasymTrajs_R4_RT1e7_dt001_si10_'+ str(runNumber)+ '.h5'
-    sim.run_n_buffer(runtime, sample=True, samplingInterval=10, \
+    outfile = '../data/asym3D/3DasymTrajs_pbox5_RT1e7_'+ dt + 'dt' + sampInterval '_si_' + str(runNumber)+ '.h5'
+    sim.run_n_buffer(runtime, sample=True, samplingInterval=sampInterval, \
                      filename = outfile, buffersize = int(1e3))
 
 #run_simulation(2)
